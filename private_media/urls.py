ee1e2507a6f561c03b59363741260516e640efab
Thomas Couderette <thomas.couderette@sodibur.com>
2016-08-18 17:48:30 +0400

    Added the view in the imports.

--- private_media/urls.py
+++ private_media/urls.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.conf.urls import url
 
+from .views import serve_private_file
+
 urlpatterns = [
     url(r'^{0}(?P<path>.*)$'.format(settings.PRIVATE_MEDIA_URL.lstrip('/')),
-        'serve_private_file', ),
+        serve_private_file, ),
 ]
