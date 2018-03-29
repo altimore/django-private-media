5701f13ad6721d839ff8e0b143db1f5d5e6e7689
Thomas C <thomas.couderette@sodibur.com>
2018-03-29 15:02:41 +0400

    Got rid of the parenthesis on user.is_authenticated.

--- private_media/permissions.py
+++ private_media/permissions.py
@@ -1,13 +1,11 @@
-
 class DefaultPrivatePermissions(object):
-    
     def has_read_permission(self, request, path):
         """
         Just return True if the user is an authenticated staff member.
         Extensions could base the permissions on the path too.
         """
         user = request.user
-        if not user.is_authenticated():
+        if not user.is_authenticated:
             return False
         elif user.is_superuser:
             return True
@@ -15,4 +13,3 @@ class DefaultPrivatePermissions(object):
             return True
         else:
             return False
-
