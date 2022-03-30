2b38f0a07e47fe92f3d082c23f378413baaf4d68
Thomas Couderette <thomas.couderette@sodibur.com>
2022-03-30 18:55:17 +0400

    Update for Django 4.0 compatibility.

--- private_media/urls.py
+++ private_media/urls.py
@@ -1,9 +1,11 @@
 from django.conf import settings
-from django.conf.urls import url
+from django.urls import re_path
 
 from .views import serve_private_file
 
 urlpatterns = [
-    url(r'^{0}(?P<path>.*)$'.format(settings.PRIVATE_MEDIA_URL.lstrip('/')),
-        serve_private_file, ),
+    re_path(
+        r"^{0}(?P<path>.*)$".format(settings.PRIVATE_MEDIA_URL.lstrip("/")),
+        serve_private_file,
+    ),
 ]
