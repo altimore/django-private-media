3bede69884b7c8ef37a50eecba83c178f473e330
Thomas Couderette <thomas.couderette@sodibur.com>
2024-09-02 14:34:27 +0400

    bump: version 0.1.5 → 0.1.6

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.1.6 (2024-09-02)
+
+### Fix
+
+- compatibility with django 4.1.6
--- private_media/views.py
+++ private_media/views.py
@@ -1,28 +1,28 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+import logging
+
 from django.conf import settings
 from django.core.exceptions import PermissionDenied
 from django.http import Http404
-from django.core.files.base import File
-from django.core.files.storage import get_storage_class
-from . import servers
 
-import logging
 logger = logging.getLogger(__name__)
 
 from importlib import import_module
+
 
 def get_class(import_path=None):
     """
     Largely based on django.core.files.storage's get_storage_class
     """
     from django.core.exceptions import ImproperlyConfigured
+
     if import_path is None:
-        raise ImproperlyConfigured('No class path specified.')
+        raise ImproperlyConfigured("No class path specified.")
     try:
-        dot = import_path.rindex('.')
+        dot = import_path.rindex(".")
     except ValueError:
         raise ImproperlyConfigured("%s isn't a module." % import_path)
-    module, classname = import_path[:dot], import_path[dot+1:]
+    module, classname = import_path[:dot], import_path[dot + 1 :]
     try:
         mod = import_module(module)
     except ImportError as e:
@@ -30,14 +30,21 @@ def get_class(import_path=None):
     try:
         return getattr(mod, classname)
     except AttributeError:
-        raise ImproperlyConfigured('Module "%s" does not define a "%s" class.' % (module, classname))
+        raise ImproperlyConfigured(
+            'Module "%s" does not define a "%s" class.' % (module, classname)
+        )
 
 
-server = get_class(settings.PRIVATE_MEDIA_SERVER)(**getattr(settings, 'PRIVATE_MEDIA_SERVER_OPTIONS', {}))
-if hasattr(settings,'PRIVATE_MEDIA_PERMISSIONS'):
-    permissions = get_class(settings.PRIVATE_MEDIA_PERMISSIONS)(**getattr(settings, 'PRIVATE_MEDIA_PERMISSIONS_OPTIONS', {}))
+server = get_class(settings.PRIVATE_MEDIA_SERVER)(
+    **getattr(settings, "PRIVATE_MEDIA_SERVER_OPTIONS", {})
+)
+if hasattr(settings, "PRIVATE_MEDIA_PERMISSIONS"):
+    permissions = get_class(settings.PRIVATE_MEDIA_PERMISSIONS)(
+        **getattr(settings, "PRIVATE_MEDIA_PERMISSIONS_OPTIONS", {})
+    )
 else:
     from .permissions import DefaultPrivatePermissions
+
     permissions = DefaultPrivatePermissions()
 
 
@@ -45,10 +52,10 @@ def serve_private_file(request, path):
     """
     Serve private files to users with read permission.
     """
-    logger.debug('Serving {0} to {1}'.format(path, request.user))
+    logger.debug("Serving {0} to {1}".format(path, request.user))
     if not permissions.has_read_permission(request, path):
         if settings.DEBUG:
             raise PermissionDenied
         else:
-            raise Http404('File not found')
+            raise Http404("File not found")
     return server.serve(request, path=path)
